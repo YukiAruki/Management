153d2665fc6f750f9c260d88807e7660174b69d5
YukiAruki <T_C6H12O6@163.com>
2022-06-18 08:34:15 +0800

    20220618_2

--- src/components/common/CommonForm.vue
+++ src/components/common/CommonForm.vue
@@ -41,7 +41,7 @@
       </el-radio-group>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit">确认</el-button>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
       <el-button @click="cancel">取消</el-button>
     </el-form-item>
   </el-form>
